723e752c371fe41a2587563de388c6878a5a0d9a
LianMuradyan1990 <muradyanlian90@gmail.com>
2024-07-25 14:52:42 +0400

    new_update

--- src/Components/All.js
+++ src/Components/All.js
@@ -32,7 +32,7 @@ export default function All() {
         </select>
       </div>
       <div className="my-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {filteredAll.slice(0, count).map((item, index) => {
+      {filteredAll.slice(0, count).map((item, index) => {
           return (
             <div key={index} className="">
               <Link
